7009fd82a8317852b1c14c12a2510a1558987078
gghatzz <hatzzgg@hotmail.com>
2020-12-22 13:48:58 +0200

    cursor

--- main.js
+++ main.js
@@ -506,3 +506,32 @@ var VanillaTilt = (function () {
   return VanillaTilt;
   
   }());
+
+
+
+
+  const cursor = document.querySelector("#cursor"),
+  anchor = document.querySelectorAll("a");
+
+//   custom cursor
+
+window.addEventListener("mousemove", (e) => {
+  let x = e.pageX,
+    y = e.pageY;
+
+  cursor.style.left = `${x}px`;
+  cursor.style.top = `${y}px`;
+});
+
+// anchor tag hover effect
+
+anchor.forEach((anc) => {
+  anc.addEventListener("mouseover", () => {
+    cursor.style.transform = "scale(2)";
+    cursor.style.animationName = "borderAnim";
+  });
+  anc.addEventListener("mouseleave", () => {
+    cursor.style.transform = "";
+    cursor.style.animationName = "";
+  });
+});
